2d3929f35bd67b0c9438e4852871bd60b3a4e3e0
Michael Chang <machang@andrew.cmu.edu>
2018-11-26 10:11:25 -0500

    fixed model

--- capstone/ChordialMusic/models.py
+++ capstone/ChordialMusic/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 
 class ChordProgression(models.Model):
-    songName = models.CharField()
-    chords = models.CharField()
+    songName = models.CharField(max_length=200)
+    chords = models.CharField(max_length=600)
